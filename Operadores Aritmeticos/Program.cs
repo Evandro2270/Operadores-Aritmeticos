74aa05f514f4ed9ba8344481df8527fa8a5d7618
Evandro2270 <evandro535@gmail.com>
2024-12-03 14:56:05 -0300

    Adicionar arquivos de projeto.

---
+++ Operadores Aritmeticos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+                int n1 = 3 + 4 * 2;
+                int n2 = (3 + 4) * 2;
+                int n3 = 17 % 3;
+                double n4 = 10.0 / 8;
+
+                Console.WriteLine(n1);
+                Console.WriteLine(n2);
+                Console.WriteLine(n3);
+                Console.WriteLine(n4);
+
+            }
+        }
+    }
+}
